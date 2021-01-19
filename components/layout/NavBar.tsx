bfbf03360f581af9a64203802bf1845f30c0fd94
Chris Dillon <chrispauldillon@live.com>
2021-01-19 20:27:36 +0000

    Finally fixed mobile view?

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Box, Flex, useColorMode, Stack, useDisclosure, Avatar, useToken, useToast, IconButton } from '@chakra-ui/core';
+import React from 'react';
+import { Box, Flex, useColorMode, Stack, useDisclosure, Avatar, IconButton } from '@chakra-ui/core';
 import { Banner } from '../common/Texts';
 import { useSelector } from 'react-redux';
 import { IAppState } from '../../redux/store';
@@ -9,79 +9,22 @@ import RightNav from './RightMenu';
 import { PbDrawerForm } from '../common/Drawers';
 import { LoginModal } from '../shared/Modals';
 import MenuBase from '../common/Menus';
-import { PbPrimaryButton } from '../common/Buttons';
-import { RiAdminLine, FaHistory, FaUserFriends, BsFillGrid3X3GapFill, BsCalendarFill, MdMenu } from 'react-icons/all';
-import { WORKOUT_DIARY_URL, TEMPLATES_URL, LOGHISTORY_URL, USERS_URL, ADMIN_URL } from '../../InternalLinks';
+import { MdMenu } from 'react-icons/all';
 import { IconType } from 'react-icons';
 import PbIconButton from '../common/IconButtons';
-import Axios from 'axios';
-import { CreateWorkoutDayUrl, GetWorkoutDayIdByDateUrl } from '../../api/account/workoutDay';
-import { ModalBackForward, ModalForward } from '../common/Modals';
 import TemplateSearchBar from '../templatePrograms/TemplateSearchBar';
 import theme from '../../theme';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { ICreateWorkoutDayOptions } from 'powerbuddy-shared';
-import { isMobile } from 'react-device-detect';
-
-const sideMenu = {
-  groups: [
-    // {
-    //   name: 'Diary',
-    //   icon: BsCalendarFill,
-    //   link: DIARY_URL,
-    //   tooltip: 'View Your Program Log',
-    //   memberStatusId: 0,
-    // },
-    {
-      name: 'New Diary',
-      icon: BsCalendarFill,
-      link: '/',
-      tooltip: 'View Your Program Log',
-      memberStatusId: 0,
-    },
-    {
-      name: 'Templates',
-      icon: BsFillGrid3X3GapFill,
-      link: TEMPLATES_URL,
-      tooltip: 'View Program Templates',
-      memberStatusId: 0,
-    },
-    {
-      name: 'History',
-      icon: FaHistory,
-      link: LOGHISTORY_URL,
-      tooltip: 'View Your log history',
-      memberStatusId: 0,
-    },
-    {
-      name: 'Users',
-      icon: FaUserFriends,
-      link: USERS_URL,
-      tooltip: 'View Active Users',
-      memberStatusId: 0,
-    },
-    {
-      name: 'Admin',
-      icon: RiAdminLine,
-      link: ADMIN_URL,
-      tooltip: 'Admin Panel',
-      memberStatusId: 4,
-    },
-  ],
-};
 
 interface INavBarProps {
   menuOpen: boolean;
   setMenuOpen: any;
 }
 
-const NavBar: React.FC<INavBarProps> = ({ menuOpen }) => {
+const NavBar: React.FC<INavBarProps> = ({}) => {
   const { user } = useSelector((state: IAppState) => state.state);
-  const router = useRouter();
-  const toast = useToast();
   const { colorMode } = useColorMode();
-  const { SCREEN_MOBILE, SCREEN_DESKTOP } = useScreenSizes();
+  const { SCREEN_MOBILE } = useScreenSizes();
   const { isAuthenticated } = useSelector((state: IAppState) => state.state);
 
   const { isOpen: isLeftNavOpen, onOpen: onLeftNavOpen, onClose: onLeftNavClose } = useDisclosure();
@@ -117,9 +60,11 @@ const NavBar: React.FC<INavBarProps> = ({ menuOpen }) => {
           </Banner>
         </Flex>
         <Flex>
-          <Box mt={2} display={SCREEN_MOBILE ? 'none' : 'inherit'}>
-            <TemplateSearchBar />
-          </Box>
+          {!SCREEN_MOBILE && (
+            <Box mt={2}>
+              <TemplateSearchBar />
+            </Box>
+          )}
           {SCREEN_MOBILE ? (
             <Box px={2}>
               <Avatar size="sm" name={user.userName!} onClick={isAuthenticated ? onMobileOpen : onLoginOpen} />
