b19db79e9890ba777df3db8acef3ceff9dff4738
Chris Dillon <chrispauldillon@live.com>
2021-01-21 23:26:01 +0000

    removed dated interface for axios calls

--- redux/util/authorization.tsx
+++ redux/util/authorization.tsx
@@ -1,10 +1,5 @@
 import axios from 'axios';
 
-export interface AxiosCall {
-  setSuccess: React.Dispatch<React.SetStateAction<boolean>>;
-  setError: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
 export const setAuthorizationToken = (token: string | null) => {
   if (token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
