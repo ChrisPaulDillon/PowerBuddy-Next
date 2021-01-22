1e18d74ae3970c39bd679c536eade5aa9dc1b2c1
Chris Dillon <chrispauldillon@live.com>
2021-01-22 18:35:57 +0000

    temp live to determine if access token is retrieved

--- components/account/Facebook.tsx
+++ components/account/Facebook.tsx
@@ -1,18 +1,27 @@
+import { Box, Button } from '@chakra-ui/core';
 import React from 'react';
 import FacebookLogin from 'react-facebook-login';
-import { TiSocialFacebookCircular } from 'react-icons/all';
+import { FaFacebook } from 'react-icons/all';
 
 export const Facebook = () => {
-  const responseFacebook = () => {};
+  const handleFacebookLogin = (response) => {
+    const { accessToken } = response;
+    console.log(accessToken);
 
+    //this.props.loginWithFacebook(accessToken);
+  };
+
+  const appId = 399640227879888;
   return (
-    <FacebookLogin
-      appId="399640227879888"
-      autoLoad
-      fields="name,email,picture"
-      onClick={responseFacebook}
-      callback={responseFacebook}
-      icon="fa-facebook"
-    />
+    <Box>
+      <FacebookLogin
+        appId="399640227879888"
+        autoLoad
+        fields="name,email,picture"
+        onClick={handleFacebookLogin}
+        callback={handleFacebookLogin}
+        icon="fa-facebook"
+      />
+    </Box>
   );
 };
