6703014d9c5fc1dbb49ee66d018011a9c1b632a9
Chris Dillon <chrispauldillon@live.com>
2021-01-23 18:29:58 +0000

    Reverted workout diary url

--- InternalLinks.ts
+++ InternalLinks.ts
@@ -1,6 +1,6 @@
 
 export const TEMPLATES_URL = '/templates';
-export const WORKOUT_DIARY_URL = '/';
+export const WORKOUT_DIARY_URL = '/workoutdiary';
 export const PORTAL_URL = '/portal';
 export const USERS_URL = '/u';
 export const LOGHISTORY_URL = '/history';
