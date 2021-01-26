e1e0327c76b8abf70a815ab4bce15172a24f85c3
Chris Dillon <chrispauldillon@live.com>
2021-01-26 23:19:58 +0000

    Added users to sitemap

--- server.js
+++ server.js
@@ -7,39 +7,32 @@ const dev = process.env.NODE_ENV !==  'production';
 const app =  next({ dev });
 const handle = app.getRequestHandler();
 
-
-try {
-  fs.readFileSync(__dirname + "/.next/serverless/pages");
-  sitemap({
-    //   alternateUrls: {
-    //     en: 'https://example.en',
-    //     es: 'https://example.es',
-    //     ja: 'https://example.jp',
-    //     fr: 'https://example.fr',
-    //   },
-      baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
-      ignoredPaths: ['admin', 'templates/[templateProgramId]', 'u/[userName]', 'personalbests/[exerciseId]', 'exercises/[exerciseId]', 'workoutdiary/[workoutDayId]'],
-      pagesDirectory: __dirname + "/.next/serverless/pages",
-      targetDirectory : 'public/static/',
-      sitemapFilename: 'sitemap.xml',
-      nextConfigPath: __dirname + "\\next.config.js"
-    });
-}
-catch(error) { //File doesn't exist, don't overwrite roadmap
-
-}
+// sitemap({
+//   //   alternateUrls: {
+//   //     en: 'https://example.en',
+//   //     es: 'https://example.es',
+//   //     ja: 'https://example.jp',
+//   //     fr: 'https://example.fr',
+//   //   },
+//     baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+//     ignoredPaths: ['admin', 'templates/[templateProgramId]', 'u/[userName]', 'personalbests/[exerciseId]', 'exercises/[exerciseId]', 'workoutdiary/[workoutDayId]'],
+//     pagesDirectory: __dirname + "/.next/serverless/pages",
+//     targetDirectory : 'public/static/',
+//     sitemapFilename: 'sitemap.xml',
+//     nextConfigPath: __dirname + "\\next.config.js"
+//   });
 
 app.prepare().then(()  =>  {
-  createServer((req,  res)  =>  {
+  createServer((req, res)  =>  {
     const parsedUrl = parse(req.url, true);
     const  { pathname, query } = parsedUrl;
-    if (pathname  ===  '/a') {
-      app.render(req,  res,  '/a',  query)
+    if (pathname === '/a') {
+      app.render(req, res, '/a', query)
     }  
-    else  if (pathname  ===  '/b') {
-      app.render(req,  res,  '/b',  query)
-    }  else  {
-      handle(req,  res,  parsedUrl)
+    else if (pathname  ===  '/b') {
+      app.render(req, res, '/b', query)
+    } else {
+      handle(req, res, parsedUrl)
   }}).listen(3000,  (err)  =>  {
     if (err) throw  err
     console.log('> Ready on http://localhost:3000')
