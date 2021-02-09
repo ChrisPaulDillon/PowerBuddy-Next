3e92c7ee16a74a99f2a45cd8bc1290989d92278d
Chris Dillon <chrispauldillon@live.com>
2021-02-09 16:42:43 +0000

    Added logo

--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import Image from 'next/image';
+import CSS from 'csstype';
+
+const divStyle: CSS.Properties = {
+  justifyContent: 'center',
+  alignContent: 'center',
+};
 
 const Logo = () => {
-  return <Image width="100px" height="50px" src="/../../images/logo.png" />;
+  return (
+    <div
+      style={{
+        position: 'relative',
+        left: '100%',
+      }}>
+      <Image width="125px" height="125px" src="/logo.png" />
+    </div>
+  );
 };
 
 export default Logo;
